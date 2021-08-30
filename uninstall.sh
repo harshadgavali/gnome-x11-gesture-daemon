3a25eba50e4390f728fa5df699eb20380f0d109e
Swastik Dwivedi <50874358+drunckj@users.noreply.github.com>
2021-08-30 17:32:08 +0530

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+systemctl --user stop gesture_improvements_gesture_daemon.service
+sudo rm  ~/.config/systemd/user/gesture_improvements_gesture_daemon.service
+sudo rm  ~/.local/bin/gesture_improvements_gesture_daemon
