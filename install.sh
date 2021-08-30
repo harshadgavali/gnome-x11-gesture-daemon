6a632920a0e5ec90aa8cd795d657b36c47bc7398
Swastik Dwivedi <50874358+drunckj@users.noreply.github.com>
2021-08-30 16:25:56 +0530

    adding necessary permissions
    adding execution permissions to service and binary. And starting the service

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 
 sed -i "s#@HOME#${HOME}#g" gesture_improvements_gesture_daemon.service
 mkdir -p ~/.config/systemd/user ~/.local/bin
+chmod +x gesture_improvements_gesture_daemon.service
+chmod +x target/release/gesture_improvements_gesture_daemon
 cp -f gesture_improvements_gesture_daemon.service ~/.config/systemd/user
 cp -f target/release/gesture_improvements_gesture_daemon ~/.local/bin
+systemctl --user start gesture_improvements_gesture_daemon.service
