5bae4c708138cbe966cd08c5695f7d1aaa7bd9db
Swastik Dwivedi <50874358+drunckj@users.noreply.github.com>
2021-08-30 17:32:30 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 systemctl --user stop gesture_improvements_gesture_daemon.service
 sudo rm  ~/.config/systemd/user/gesture_improvements_gesture_daemon.service
 sudo rm  ~/.local/bin/gesture_improvements_gesture_daemon
